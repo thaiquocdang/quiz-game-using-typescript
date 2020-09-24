b54e486dcfe4ccf250c071683c968385ce7ca324
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2020-09-24 18:30:07 +1000

    complete API & Basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+import { fetchQuizQuestions, Difficulty, QuestionState} from './API'
 
-function App() {
+const TOTAL_QUESTION = 2;
+
+type AnswerObject = {
+  question: string;
+  userAnswer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  // console.log(fetchQuizQuestions(TOTAL_QUESTION, Difficulty.HARD));
+  console.log(questions);
+  
+  //trigger the game
+  const startQuestion = async () => {
+    console.log('start question');
+
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTION,
+      Difficulty.MEDIUM
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('user chose an answer');
+  }
+
+  const nextQuestion = () => {
+    if (number < TOTAL_QUESTION){
+      setNumber(number+1)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QUIZZZ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTION ? (
+        <button className='start' onClick={startQuestion}>Start</button>
+      ) : null}
+
+      {!gameOver ? <p className='score'>Score:</p> : null}
+      {loading ? <p className='loading'>Loading questions</p> : null}
+      
+      { !loading && !gameOver && (
+        <QuestionCard
+           questionNumber = { number + 1 }
+           totalQuestion = {TOTAL_QUESTION}
+           question = { questions[number].question }
+           answers = { questions[number].answers } 
+           userAnswer = { userAnswers ? userAnswers[number] : undefined }
+           callback = { checkAnswer }
+        />
+      )}
+      { number < TOTAL_QUESTION-1 ? <button className='next' onClick={nextQuestion}>Next Question</button> : null}
+      
+
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNumber: number;
+    totalQuestion: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestion}) => {
+    return (
+        <div>
+            <p className='number'>
+                Question: {questionNumber} / {totalQuestion}
+            </p>
+            <p dangerouslySetInnerHTML={{ __html: question}}/>
+            <div>
+                {answers.map(answer => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callback} >
+                            <span dangerouslySetInnerHTML={{ __html: answer}} />
+                        </button>
+                    </div>    
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default QuestionCard

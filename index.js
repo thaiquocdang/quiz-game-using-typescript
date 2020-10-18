1e5afa9958504573d31e3c398bc74631e2e125dd
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2020-10-18 11:27:52 +1100

    update port as env

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(5000);
-console.log('App is running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log('App is running on port ' + port);

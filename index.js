f09a197c8b5558ec209aa788589473d7e602621f
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2020-10-18 11:35:30 +1100

    Update

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-console.log('hello');
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const port = process.env.PORT || 5000;
